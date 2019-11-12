01640920877b64a2ab96ad6362109af522755238
bdziedzic <bartosz.dziedzic94@gmail.com>
2019-11-12 03:16:06 +0100

    def file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CourseComponent } from './model/entity/course/course.component';
+import { InstructorComponent } from './model/entity/instructor/instructor.component';
+import {InstructorsData} from './model/db/in-memory-db';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    CourseComponent,
+    InstructorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InstructorsData)
   ],
   providers: [],
   bootstrap: [AppComponent]
